186a02c067d369d8a3fa0b4320dd0c15ca5322a4
muhammad-ehtisham-haider <120640715+muhammad-ehtisham-haider@users.noreply.github.com>
2022-12-31 15:39:36 +0500

    change
    this is notification page, unseen notification is in gray bg-color and has number i.e 3 , and unseen notification has a red mark, when someone click on "Mark all as read"
    then background-color change into white and number turn into "0" and red mark will remove, and all notification mark as read

---
+++ script.js
@@ -0,0 +1,30 @@
+function  remove(){
+    const element = document.querySelectorAll('.noti');
+    element.forEach(  abc=> {
+        abc.classList.remove("color");
+        
+    })
+}
+
+function two(){
+    var a = document.querySelectorAll('i');
+    a.forEach( xyz=>{
+        xyz.style.display ="none";
+    })
+}
+
+
+function replace(){
+    document.getElementById("p1").innerHTML = "0";
+
+}
+
+
+
+
+
+
+
+
+
+
